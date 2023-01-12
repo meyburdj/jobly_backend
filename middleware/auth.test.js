0a951951f6427dc70e5e2ca708b37d06d5e4fbb2
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2023-01-12 15:08:02 -0800

    added tests for middleware

--- middleware/auth.test.js
+++ middleware/auth.test.js
@@ -5,12 +5,15 @@ const { UnauthorizedError } = require("../expressError");
 const {
   authenticateJWT,
   ensureLoggedIn,
+  ensureAdmin
 } = require("./auth");
 
 
 const { SECRET_KEY } = require("../config");
 const testJwt = jwt.sign({ username: "test", isAdmin: false }, SECRET_KEY);
 const badJwt = jwt.sign({ username: "test", isAdmin: false }, "wrong");
+const testAdmin = jwt.sign({ username: "admin", isAdmin: true }, SECRET_KEY);
+
 
 function next(err) {
   if (err) throw new Error("Got error from middleware");
@@ -57,6 +60,20 @@ describe("ensureLoggedIn", function () {
     const req = {};
     const res = { locals: {} };
     expect(() => ensureLoggedIn(req, res, next)).toThrowError();
+  });
+});
+
+describe("ensureAdmin", function () {
+  test("works", function () {
+    const req = {};
+    const res = { locals: { user: { isAdmin: true } } };
+    ensureAdmin(req, res, next);
+  });
+
+  test("unauth if no admin", function () {
+    const req = {};
+    const res = { locals: { user: { isAdmin: false } } };
+    expect(() => ensureAdmin(req, res, next)).toThrowError();
   });
 });
 
