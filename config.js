7a056b1b1ee926f804597af65ac1f114a55f7926
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2023-04-24 11:41:53 -0700

    updating config for deployment

--- config.js
+++ config.js
@@ -12,9 +12,16 @@ const PORT = +process.env.PORT || 3001;
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
 }
+
+// // Use dev database, testing database, or via env var, production database
+// function getDatabaseUri() {
+//   return (process.env.NODE_ENV === "test")
+//     ? "postgresql://meyburdj:meyburdj@localhost/jobly_test"
+//     : process.env.DATABASE_URL || "postgresql://meyburdj:meyburdj@localhost/jobly";
+// }
 
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
